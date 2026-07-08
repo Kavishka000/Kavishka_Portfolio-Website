3b275550f3e7c4884abf92e7ec2e4c242c371fb1
Kavindu Kavishka <kavindukavishka56@gmail.com>
2026-07-08 14:39:02 +0530

    The portfolio website is complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,14 +140,112 @@ const sendEmail = async (e) => {
       setTimeout(() => contactMessage.textContent = '', 5000)
    }
 }
-contactForm.addEventListener('submit', sendEmail)      
+contactForm.addEventListener('submit', sendEmail)
+
+
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    // Add the .scroll-header class if the bottom scroll of the viewport is greater than 350
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+// Link the ID of each section (section id="home") to each link (a href="#home")
+// and activate the link with the class .active-link
+const scrollActive = () => {
+    // We get the position by scrolling down
+    const scrollY = window.scrollY
+
+    sections.forEach(section => {
+        const id = section.id, // id of each section
+              top = section.offsetTop - 50, // Distance from the top edge
+              height = section.offsetHeight, // Element height
+              link = document.querySelector('.nav__menu a[href*=' + id + ']') // id nav link
+
+        if(!link) return
+
+        link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== CUSTOM CURSOR ===============*/
+const cursor = document.querySelector('.cursor')
+let mouseX = 0, mouseY = 0 // Store mouse position
 
+const cursorMove = () => {
+    cursor.style.left = `${mouseX}px` // Horizontal position (X-axis)
+    cursor.style.top = `${mouseY}px` // Vertical position (Y-axis)
+    cursor.style.transform = 'translate(-50%, -50%)' // Centers the element at the pointer
+
+    // Repeats the function with each movement
+    requestAnimationFrame(cursorMove)
+}
+
+// Detects mouse movement and updates positions
+document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX // Save position X
+    mouseY = e.clientY // Save position Y
+})
+
+cursorMove()
+
+/* * Hide custom cursor on links */
+const a = document.querySelectorAll('a')
+
+a.forEach(item => {
+    // Mouse enters the link and hides the cursor
+    item.addEventListener('mouseover', () => {
+        cursor.classList.add('hide-cursor')
+    })
+    // Mouse exits the link and shows the cursor
+    item.addEventListener('mouseleave', () => {
+        cursor.classList.remove('hide-cursor')
+    })
+})
 
 /*=============== SCROLLREVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 1200,
+    delay: 300,
+    easing: 'cubic-bezier(0.34, 1.56, 0.64, 1)',
+    // reset: true, // Animations repeat
+})
+
+sr.reveal(`.home__subtitle`)
+sr.reveal(`.home__title`, {delay: 600})
+sr.reveal(`.home__description`, {delay: 900})
+sr.reveal(`.home__box-1`, {delay: 1200, rotate:{z: -20}})
+sr.reveal(`.home__box-2`, {delay: 1300, rotate:{z: -30}})
+sr.reveal(`.home__box-3`, {delay: 1400, rotate:{z: -40}})
+sr.reveal(`.home__img`, {delay: 1700, distance: '-60px'})
+sr.reveal(`.home__circle`, {delay: 2000, distance: '-100px'})
+
+sr.reveal(`.about__title`)
+sr.reveal(`.about__description`, {delay: 600})
+sr.reveal(`.about__button`, {delay: 900})
+
+sr.reveal(`.work__swiper`)
+sr.reveal(`.services__card:nth-child(odd)`, {interval: 200, origin: 'left', distance: '100px'})
+sr.reveal(`.services__card:nth-child(even)`, {interval: 200, origin: 'right', distance: '100px'})
+
+sr.reveal(`.skills__description`)
+sr.reveal(`.skills__card`, {delay: 600, interval: 200})
+sr.reveal(`.skills__profession`, {delay: 900})
+sr.reveal(`.skills__list`, {delay: 1200, interval: 200})
+
+sr.reveal(`.testimonials__container`)
+
+sr.reveal(`.contact__form`)
+sr.reveal(`.contact__link`, {delay: 600, interval: 200})
+
+sr.reveal('.footer__container')
